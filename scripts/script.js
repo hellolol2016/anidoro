5d2acd8176824709df77b1988ff2dd0bb622cd4a
hellolol2016 <dennisthederp812@gmail.com>
2021-06-29 23:39:36 -0700

    day 2 night

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,35 @@
+let workdur;
+let brekdur;
+storage = window.localStorage;
+//localstorage has workdur, brekdur, theme
+if(localStorage.getItem('workdur')===null){
+    workdur = 25;
+    brekdur = 5;
+} else {
+    workdur = localStorage.getItem('workdur');
+    brekdur = localStorage.getItem('brekdur');
+}
 
+let timer = document.querySelector('.timer h1');
 
-let workdur = 1;
-let brekdur = 5;
+if(workdur > 60){
+    timer.innerText = (workdur % 60 + ":" + (workdur-60) + ":00");
+} else {
+    timer.innerText = (workdur + ":00");
+}
 
 function getDelta(curTime, endTime){
     let delta =  endTime.getTime() - curTime.getTime();
 
-    let hours = Math.floor((delta %(1000 * 60 * 60 *24)) / (1000 * 60 * 60))
+    let hours = Math.floor((delta-1 %(1000 * 60 * 60 *24)) / (1000 * 60 * 60))
     let minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((delta % (1000 * 60)) / 1000);
     if(delta = 0){
         
     }
+
+    minutes = twoZero(minutes);
+    seconds = twoZero(seconds);
     if(hours === 0){
         if(minutes === 0){
             if(seconds === 0){
@@ -19,17 +37,20 @@ function getDelta(curTime, endTime){
             }
             return(seconds+"s");
         }
-        return(minutes + "m" + seconds+"s");
+        return(minutes + ":" + seconds);
     }
-    return(hours + "h" + minutes + "m" + seconds+"s");
+    return(hours + ":" + minutes + ":" + seconds);
 }
 
+function twoZero(n){
+    return n > 9 ? "" + n: "0" + n;
+}
 
 function workTimer(){
     startTimer(workdur);
 }
 
-let timer = document.querySelector('.timer h1');
+
 function startTimer(dur){
     let durInMin = dur;
     let startTime = new Date();
@@ -57,6 +78,7 @@ function getWorkOption(){
     let work = document.querySelector(".work-select");
     let time = work.options[work.selectedIndex].value;
     console.log(time);
+    
     return time;
 }
 
@@ -64,11 +86,13 @@ function setWork(selected){
     console.log('setwork')
     selected.selected = true;
     workdur = getWorkOption()
+    localStorage.setItem('workdur', workdur)
 }
 
 function getBreakOption(){
     let brek = document.querySelector(".break-select");
     let time = brek.options[brek.selectedIndex].value;
+    
     console.log(time);
     return time;
 }
@@ -76,6 +100,7 @@ function setBreak(selected){
     console.log('setbrek')
     selected.selected = true;
     brekdur = getBreakOption()
+    localStorage.setItem('workdur', workdur)
 }
 
 const go = document.querySelector(".Go");
@@ -83,3 +108,20 @@ go.addEventListener('click', ()=>{
     setWork(getWorkOption());
     setBreak(getBreakOption());
 });
+
+
+
+// work & rest button selection
+workButton = document.querySelector('.work_grind');
+breakButton = document.querySelector('.break_time');
+
+workButton.addEventListener('click', ()=>{
+    workButton.classList.add('selected');
+    breakButton.classList.remove('selected')
+})
+
+breakButton.addEventListener('click', ()=>{
+    breakButton.classList.add('selected');
+    workButton.classList.remove('selected')
+})
+
