f8427e6d5802c4a6f5db2d3daad59eddbf34f0aa
hellolol2016 <dennisthederp812@gmail.com>
2021-07-02 09:16:19 -0700

    day 4 night

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 let workdur;
+let theTIME;
 let brekdur;
+let theme = 'opal'
+let running = false;
+let docTheme = document.getElementsByTagName('link')[0];
 storage = window.localStorage;
 //localstorage has workdur, brekdur, theme
 if(localStorage.getItem('workdur_index')===null && localStorage.getItem('breakdur_index')===null){
@@ -21,12 +25,19 @@ if(localStorage.getItem('workdur_index')===null && localStorage.getItem('breakdu
     brekdur = brek.options[brekdurInd].value;
 }
 
+if(localStorage.getItem('theme') === null){
+    localStorage.setItem('theme', "opal");
+} else {
+    let curTheme = localStorage.getItem('theme');
+    docTheme.setAttribute('href',`styles/${curTheme}.css`);
+}
+
 let timer = document.querySelector('.timer h1');
 
 const beforeUnloadListener = (event) => {
     event.preventDefault();
     return event.returnValue = "Are you sure you want to exit?";
-  };
+};
 
 if(workdur >= 60){
     timer.innerText = (workdur % 60 + ":" + (workdur-60) + ":00");
@@ -67,27 +78,32 @@ function workTimer(){
 }
 
 function breakTimer(){
-    startTimer(breakdur);
+    startTimer(brekdur);
 }
 
+
+
+let deltaOut;
 function startTimer(dur){
     let durInMin = dur;
     let startTime = new Date();
     let endTime = new Date();
     endTime.setTime(startTime.getTime() + (durInMin * 60 * 1000));
-
     
-    setInterval(function run(){
+     theTIME = setInterval(function run(){
+
         let curTime = new Date();
         let curSec = curTime.getSeconds();
         curTime.setSeconds(curSec - 2)
-        let deltaOut = getDelta(curTime, endTime);
+        deltaOut = getDelta(curTime, endTime);
         if(deltaOut === "TERMINATE"){
             //end code
+            timer.textContent = brek.options[brek.selectedIndex].value + ":00";
             removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
         }
         timer.innerText = deltaOut;
         console.log(deltaOut);
+    
     },1001);
     
     const beforeUnloadListener = (event) => {
@@ -149,23 +165,56 @@ function setBreak(selected){
     localStorage.setItem('brekdur_index',brekind)
 }
 
-const go = document.querySelector(".Go");
-go.addEventListener('click', ()=>{
+
+function restartTimer(){
+    brek.options[brek.selectedIndex].value
+}
+
+
+const set = document.querySelector(".Go");
+set.addEventListener('click', ()=>{
     setWork(getWorkOption());
     setBreak(getBreakOption());
 });
 
+
 const startButton = document.querySelector('.start_timer');
 startButton.addEventListener('click', ()=>{
-    let mode = document.querySelector('.selected');
-    if(mode.value=== "work"){
-        workTimer();
-    } else if(mode.value === "break"){
-        breakTimer();
+    if(running === false){
+        let mode = document.querySelector('.selected');
+        if(mode.value=== "work"){
+            workTimer();
+        } else if(mode.value === "break"){
+            breakTimer();
+        }
+        running = true;
     }
+})
+
+const restartButton = document.querySelector('.restart_timer');
+restartButton.addEventListener('click', ()=>{
 
 })
 
+const themeButtons = document.querySelectorAll('.theme-button');
+
+if (docTheme.getAttribute("href") == "styles/styles.css"){
+    console.log('empty theme')
+    docTheme.setAttribute('href','styles/opal.css')
+}
+themeButtons.forEach((themeButton)=>{
+    themeButton.addEventListener('click',()=>{
+        newTheme = themeButton.value;
+        localStorage.setItem('theme', newTheme);
+        console.log("new theme is" + newTheme)
+        // Change the value of href attribute 
+        // to change the css sheet.
+        docTheme.setAttribute('href',`styles/${newTheme}.css`);
+    })
+})
+
+let brek = document.querySelector(".break_select");
+let work = document.querySelector(".work_select");
 
 // work & rest button selection
 workButton = document.querySelector('.work_grind');
@@ -174,11 +223,12 @@ breakButton = document.querySelector('.break_time');
 workButton.addEventListener('click', ()=>{
     workButton.classList.add('selected');
     breakButton.classList.remove('selected')
+    timer.textContent = work.options[work.selectedIndex].value + ":00";
 })
 
 breakButton.addEventListener('click', ()=>{
     breakButton.classList.add('selected');
     workButton.classList.remove('selected')
-    timer.textContent = work.options[work.selectedIndex].value;
+    timer.textContent = brek.options[brek.selectedIndex].value + ":00";
 })
 
